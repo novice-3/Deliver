acd6727c1a49a22eeecee26b10f18b82e9de5043
Johan <23b29.johan@sjec.ac.in>
2025-01-28 21:17:52 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap App component in BrowserRouter */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
